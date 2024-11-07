cc1da8c9ff08729dc17299926820bf9b5544f7bd
fender5234 <fender5234@gmail.com>
2024-11-07 17:19:04 +0300

    Небольшие правки стилей

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   const sliderWrapperFirst = document.querySelector('.slider1');
+  const sliderWrapperSecond = document.querySelector('.slider2');
+
   const splide1 = new Splide(sliderWrapperFirst, {
     mediaQuery: 'min',
+    perPage: 1,
+    pagination:false,
+    arrows:false,
+    autoWidth:true,
     breakpoints: {
       768: {
         destroy: true,
@@ -9,4 +15,24 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
   splide1.mount();
+
+  const splide2 = new Splide(sliderWrapperSecond, {
+    type: 'loop',
+    speed: 600,
+    pagination:false,
+    arrows:false,
+    mediaQuery: 'min',
+    breakpoints: {
+      768: {
+        gap: 30,
+        perPage: 1,
+      },
+      1024: {
+        gap: 30,
+        perPage: 3,
+      },
+    
+    }
+  });
+  splide2.mount();
 });
